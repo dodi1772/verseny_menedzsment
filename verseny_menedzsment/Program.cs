fb94568407e1f839bbaa1584481fadff092e8c14
dodi1772 <zsigmond.dorian01@gmail.com>
2025-05-27 08:42:36 +0200

    Add project files.

---
+++ verseny_menedzsment/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace verseny_menedzsment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
