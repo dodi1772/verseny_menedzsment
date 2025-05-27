4b6d18c694aa81871c56926f5f4bb1e0cf381100
dodi1772 <zsigmond.dorian01@gmail.com>
2025-05-27 08:44:42 +0200

    Kesz

--- verseny_menedzsment/Eredmeny.cs
+++ verseny_menedzsment/Eredmeny.cs
@@ -8,5 +8,15 @@ namespace verseny_menedzsment
 {
     internal class Eredmeny
     {
+        public string VersenyNev { get; set; }
+        public TimeSpan Ido { get; set; }
+        public int Helyezes { get; set; }
+
+        public Eredmeny(string versenyNev, TimeSpan ido, int helyezes)
+        {
+            VersenyNev = versenyNev;
+            Ido = ido;
+            Helyezes = helyezes;
+        }
     }
 }
